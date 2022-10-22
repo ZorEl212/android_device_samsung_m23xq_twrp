3a601145613c76f80fb34a30dad9f50f4eddba47
ZorEl212 <yabsirad212@gmail.com>
2022-10-22 13:11:58 -0700

    misc changes

--- device.mk
+++ device.mk
@@ -6,3 +6,19 @@
 #
 
 LOCAL_PATH := device/samsung/m23xq
+
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 30
+# API
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Dependencies
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
--- twrp_m23xq.mk
+++ twrp_m23xq.mk
@@ -7,16 +7,16 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from m23xq device
 $(call inherit-product, device/samsung/m23xq/device.mk)
 
 PRODUCT_DEVICE := m23xq
-PRODUCT_NAME := omni_m23xq
+PRODUCT_NAME := twrp_m23xq
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-E236B
 PRODUCT_MANUFACTURER := samsung
