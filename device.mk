3c279256eaa2d8c6e245c7e2880f061c39879bac
diamondsoulreturns <n.shetti65@gmail.com>
2022-11-03 17:50:43 +0530

    fastbootd

--- device.mk
+++ device.mk
@@ -11,7 +11,7 @@ LOCAL_PATH := device/samsung/m23xq
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 30
 # API
-PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -19,6 +19,11 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Dependencies
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion
+ 
+# Fastbootd    
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd    
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
