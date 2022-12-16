c26a5eea2680b71264bc808057283f39e4a39549
ZorEl212 <yabsirad212@gmail.com>
2022-12-16 01:15:32 -0500

    try decryption and multiple changes

--- device.mk
+++ device.mk
@@ -20,10 +20,25 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion
  
+ # Encryption: Use common FBE decryption script
+BOARD_USES_QCOM_FBE_DECRYPTION := true
+
+# Encryption: Setup it
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+    
 # Fastbootd    
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd    
+
+# Health 
+RODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
