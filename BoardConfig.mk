b87145c22cf915b88a7c2183f7482ddc14d27b69
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-02-22 18:18:55 -0300

    BoardConfig: Add RTC FIX
    Fixes wrong date and time on timestamps

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,10 +120,11 @@ PLATFORM_VERSION := 99
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
 TW_SCREEN_BLANK_ON_BOOT := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true # Fixes wrong date and time on timestamps
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_MAX_BRIGHTNESS := 306
-TW_DEFAULT_BRIGHTNESS := 150
+TW_DEFAULT_BRIGHTNESS := 255
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone50/temp"
 TW_Y_OFFSET := 80
