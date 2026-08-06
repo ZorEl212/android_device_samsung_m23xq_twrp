29750222bef34cac1761f33353fb37462047c70f
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2026-08-05 22:13:00 -0300

    AndroidProducts: Update header
    Updated copyright years in AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 The TWRP Open Source Project
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 The TWRP Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
